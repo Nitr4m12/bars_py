7cce14bd9721bdfa779fce224c20c3389ddac7cf
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2021-11-21 00:05:31 -0300

    Reformatted most of the code to organize it better

--- bars.py
+++ bars.py
@@ -6,16 +6,26 @@
 __author__ = "Nitr4m12(based on bars_convertor.py from Peter Wunder (@SamusAranX))"
 __license__ = "WTFPL"
 
-from utils import *
-from typing import List
-import bcf_converter as sound
+try:
+	from utils import *
+except ModuleNotFoundError:
+	from bars_py.utils import *
+
+from typing import List, Dict, Tuple, Any
 
 def plural_s(n):
 	return "s" if n != 1 else ""
 
-def get_bars_tracks(bars: bytearray, swap_endian: bool=False):
+def get_bars_tracks(bars):
 
-	if bars[0x8:0x12] == b"\xFF\xFE":
+	"""
+	Get the tracks and offsets to them inside the bars file, and return 
+	two dictionaries: one containing the tracks, with their names as keys,
+	and another one containing each track's offset, with the track names as
+	keys as well
+	"""
+
+	if bars[0x8:0xA] == b"\xFF\xFE":
 		bom = '<'
 	else:
 		bom = '>'
@@ -25,75 +35,96 @@ def get_bars_tracks(bars: bytearray, swap_endian: bool=False):
 	pos: int = 0
 
 	# Create a structure for the header
-	header: Header(bom) #= Header(bom)
+	header: Header = Header(bom)
 	header.data(bars, pos)
 
 	# Check for an invalid file
-	if header.magic != BARS_HEADER:
-		return False
-
-	if header.size_ != file_size:
+	if header.magic != b"BARS":
+		print('Invalid BARS file!')
 		return False
 
 	# Offset an amount of header.size
 	pos += header.size + header.count * 4
 
 	# Create a structure for the track structure
-	track_struct: TRKStruct(bom, header.count) #= TRKStruct(bom, header.count)
+	track_struct: TRKStruct = TRKStruct(bom, header.count)
 	track_struct.data(bars, pos)
 
-	tracks: List[bytearray]
+	tracks_data = []
+	track_names = []
+	track_offsets = []
 
-	for t in range(header.count):
+	# Look for the track name
+	for i in range(header.count):
+		# Get AMTA offset
+		pos = track_struct.offsets[i * 2] 
+
+		# Create a structure for the AMTA header
+		amta: AMTAHeader = AMTAHeader(bom)
+		amta.data(bars, pos)
+
+		if amta.magic != AMTA_HEADER:
+			print('Invalid AMTA Header!')
+			return False
+
+		# Offset an amount of amta.size
+		pos += amta.size
+
+		for j in MAGICS:
+			data: AMTASubHeader = AMTASubHeader(bom)
+			data.data(bars, pos)
+			pos += data.size
+			if j != b'STRG':
+				pos += data.length
+			else:	
+				data.data_ = bars[pos:pos + data.length]
+				track_names.append((data.data_).decode('utf-8').split('\0')[0])
+
+	for i in range(header.count):
 		# Get the offset of our track
-		pos = track_struct.offsets[t * 2 + 1] 
+		pos = track_struct.offsets[i * 2 + 1] 
 
-		if bars_track_offset >= header.size_: 
+		if pos >= header.size_: 
 			# The offset the file is telling us to jump to can't exist because the file's too small
 			continue
 
-		fwav = FWAVHeader(bom)
+		fwav: FWAVHeader = FWAVHeader(bom)
 		fwav.data(bars, pos)
 
 		fwav.data_ = bars[pos:pos + fwav.size_]
 
-		tracks.append(fwav.data_)
+		tracks_data.append(fwav.data_)
+		track_offsets.append(pos)
 
-	return tracks
+	tracks = dict(zip(track_names, tracks_data))
+	offsets = dict(zip(track_names, track_offsets))
 
-def convert_track(track: bytearray, dest_bom: str):
-	# Read the header of our track
-	dest: str = track[:0x4].decode('utf-8')
+	return tracks, offsets
 
-	# Run the track through the converter
-	new_track: bytearray = sound.convFile(track, dest, dest_bom)
-	track = new_track
+def convert_bars(bars, dest_bom):
 
-	return track
-		
-def read_bars(bars, dest_bom):
+	"""
+	Convert a bars file between endians
+	"""
 
-	if bars[0x8:0x12] == b"\xFF\xFE":
+	if bars[0x8:0xA] == b"\xFF\xFE":
 		bom = '<'
 	else:
 		bom = '>'
 
 	# Create an output buffer the length of our file
-	output_buffer = bytearray(len(f))
+	output_buffer = bytearray(len(bars))
 	pos = 0
 
 	# Create a structure for the header
 	header = Header(bom)
-	header.data(f, pos)
+	header.data(bars, pos)
 
 	# Check for an invalid file
 	if header.magic != BARS_HEADER:
 		raise RuntimeError(f"Not a valid BARS file.")
 
-	file_size = len(f)
-	if header.size_ != file_size:
-		# raise RuntimeError(f"File size mismatch (expected {header.size_} bytes, got {file_size} bytes)")
-		pass
+	file_size = len(bars)
 
 	# Write the header data to the output buffer
 	output_buffer[pos:pos + header.size] = bytes(
@@ -103,28 +134,28 @@ def read_bars(bars, dest_bom):
 	# Offset an amount of header.size
 	pos += header.size
 
-	trk_info = TRKInfo(bom, header.count)
-	trk_info.data(f, pos)
-	output_buffer[pos:pos + trk_info.size] = TRKInfo(dest_bom, header.count).pack(*trk_info.unknown)
-	pos += trk_info.size
+	# Copy the unknown data portion to the output_buffer
+	unknown = Unknown(bom, header.count)
+	unknown.data(bars, pos)
+	output_buffer[pos:pos + unknown.size] = Unknown(dest_bom, header.count).pack(*unknown.unknown)
+	pos += unknown.size
 
 	# Create a structure for the track structure
 	track_struct = TRKStruct(bom, header.count)
-	track_struct.data(f, pos)
+	track_struct.data(bars, pos)
 
 	# Write the track structure data to the output buffer
 	output_buffer[pos:pos + track_struct.size] = TRKStruct(dest_bom, header.count).pack(*track_struct.offsets)
 
 	track_names = []
 	
-	for t in range(header.count):
-		amta_offset = track_struct.offsets[t * 2] # Get AMTA offset from list
-		# Position ourselves at the AMTA offset
-		pos = amta_offset
+	for i in range(header.count):
+		# Get AMTA offset from list and position ourselves at that point
+		pos = track_struct.offsets[i * 2] 
 
 		# Create a structure for the AMTA header
 		amta = AMTAHeader(bom)
-		amta.data(f, pos)
+		amta.data(bars, pos)
 
 		if amta.magic != AMTA_HEADER:
 			raise RuntimeError(f"Track {t+1} has an invalid AMTA header")
@@ -137,31 +168,31 @@ def read_bars(bars, dest_bom):
 		# Offset an amount of amta.size
 		pos += amta.size
 
-		for i in MAGICS:
+		for j in MAGICS:
 
 			data = AMTASubHeader(bom)
-			data.data(f, pos)
+			data.data(bars, pos)
 				
 			output_buffer[pos:pos + data.size] = bytes(AMTASubHeader(dest_bom).pack(data.magic, data.length))
 			pos += data.size
 
-			data.data_ = f[pos:pos + data.length]
+			data.data_ = bars[pos:pos + data.length]
 			start = 0
-			if i == b'DATA':
-				output_buffer[pos:pos + 12] = f[pos:pos + 12]
+			if j == b'DATA':
+				output_buffer[pos:pos + 12] = bars[pos:pos + 12]
 				start = 12
 			
-			if bom != dest_bom and i != b'STRG':
-				for j in range(start, len(data.data_), 4):
-					output_buffer[pos + j:pos + j + 4] = bytes([
-						data.data_[j+3], data.data_[j+2], data.data_[j+1], data.data_[j] # Do an endian swap
+			if bom != dest_bom and j != b'STRG':
+				for k in range(start, len(data.data_), 4):
+					output_buffer[pos + k:pos + k + 4] = bytes([
+						data.data_[k+3], data.data_[k+2], data.data_[k+1], data.data_[k] # Do an endian swap
 					])
 			elif bom != dest_bom:
 				output_buffer[pos:pos + data.length] = data.data_
 
 			pos += data.length
 		
-			if data.magic != i:
+			if data.magic != j:
 				raise RuntimeError(f"Track {t+1} has an invalid {data.magic} header")
 	# Print out the number of tracks found
 	print(f"{header.count} track{plural_s(header.count)} found!")
@@ -169,21 +200,17 @@ def read_bars(bars, dest_bom):
 	if pos == header.size_: # We have now reached the end of the file despite the file telling us that there would be stuff here
 		raise RuntimeError(f"Reached EOF, this file probably doesn't actually contain any FWAVs despite containing the offsets for them")
 
-	for t in range(header.count):
-		bars_track_offset = track_struct.offsets[t * 2 + 1] # Get track offset from list
-		if bars_track_offset >= header.size_: # The offset the file is telling us to jump to can't exist because the file's too small
-			# print(f"{filename}: Track {t+1} probably doesn't exist, skipping it")
+	for i in range(header.count):
+		pos = track_struct.offsets[i * 2 + 1] # Get track offset from list
+		if pos >= header.size_:
+			# The offset the file is telling us to jump to can't exist because the file's too small
 			continue
 
-		pos = bars_track_offset
-
 		fwav = FWAVHeader(bom)
-		fwav.data(f, pos)
+		fwav.data(bars, pos)
 
-		fwav_converted = sound.convFile(f[pos:pos + fwav.size_], sound.bytes_to_string(fwav.magic), dest_bom)
+		output_buffer[pos:pos + fwav.size_] = bars[pos:pos + fwav.size_]
 
-		output_buffer[pos:pos + fwav.size_] = bytes(fwav_converted)
-
-	output_buffer[4:8] = struct.pack(dest_bom + "I", len(output_buffer))
+	output_buffer[0x4:0x8] = struct.pack(dest_bom + "I", len(output_buffer))
 
 	return output_buffer
